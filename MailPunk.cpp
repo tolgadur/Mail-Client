b61bd50d109b92164ff95d51a78f40c7a55df100
Tolga Dur <durtolga1@gmail.com>
2019-01-08 15:54:32 +0000

    MailPunk.cpp

--- MailPunk.cpp
+++ MailPunk.cpp
@@ -1,3 +1,21 @@
+CC=g++ -Wall
+
+all: main
+
+main: helper.o main.o
+	$(CC) -pthread -o main helper.o main.o
+
+main.o: helper.cc main.cc
+	$(CC) -c helper.cc main.cc
+
+queue.o: queue.cc
+	$(CC) -c queue.cc
+
+tidy:
+	rm -f *.o core
+
+clean:
+	rm -f main producer consumer *.o core
 #include "imap.hpp"
 #include "UI.hpp"
 #include <memory>
